084c43df3c57fc741237a49e8cdbfc65fb1da53a
Isaias Lagunes Pérez <isaiasaries@hotmail.com>
2022-08-24 13:18:07 -0500

    Commit Inicial, incluye conexion a Mysql a BD springapp

---
+++ src/main/java/com/lagunes/DemoWebTotal1Application.java
@@ -0,0 +1,13 @@
+package com.lagunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebTotal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebTotal1Application.class, args);
+	}
+
+}
